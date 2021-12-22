270988a8a0dafa4cc7bc825581240113252e1150
Ram G Athreya <49661809+ram-viv-labs@users.noreply.github.com>
2021-12-22 11:29:02 -0500

    Fix QANARY endpoint

--- src/main/java/chatbot/lib/api/qa/QANARY.java
+++ src/main/java/chatbot/lib/api/qa/QANARY.java
@@ -27,7 +27,7 @@ import java.util.Map;
  */
 public class QANARY {
     private static final Logger logger = LoggerFactory.getLogger(QANARY.class);
-    private static final String URL = "http://wdaqua-core1.univ-st-etienne.fr/gerbil";
+    private static final String URL = "http://wdaqua-core1.univ-st-etienne.fr/api/gerbil";
 
     private HttpClient client;
 
