3be09eecea45f39660beeba07736e474debe2791
Madan H K <109542799+madanhk18@users.noreply.github.com>
2025-03-02 20:27:52 +0530

    Fixed errors in #45

--- src/main/java/chatbot/lib/request/RequestType.java
+++
@@ -1,9 +0,0 @@
-package chatbot.lib.request;
-
-/**
- * Created by ramgathreya on 5/19/17.
- */
-public interface RequestType {
-    String TEXT_MESSAGE = "text";
-    String PARAMETER_MESSAGE = "parameter";
-}
